8173ab8978f78421af9a1466537833be802384b4
Vladyslav Dikhtiaruk <vladyslav.dikhtiaruk@hva.nl>
2023-11-06 17:44:32 +0100

    finished PlayScreen

--- app/src/main/java/nl/hva/madlevel6task2/ui/screens/PlayScreen.kt
+++ app/src/main/java/nl/hva/madlevel6task2/ui/screens/PlayScreen.kt
@@ -1,5 +1,6 @@
 package nl.hva.madlevel6task2.ui.screens
 
+import android.app.Application
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -7,27 +8,50 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import nl.hva.madlevel6task2.R
-import java.time.format.TextStyle
+import nl.hva.madlevel6task2.model.Game
+import nl.hva.madlevel6task2.viewModel.GameViewModel
+import java.util.Date
+
 
 @Composable
-fun PlayScreen(modifier : Modifier, navController : NavController) {
+fun PlayScreen(modifier : Modifier, navController : NavController, viewModel : GameViewModel) {
+    // Create a LiveData to observe the selected image
+    val userSelectedImage = remember { mutableStateOf(viewModel.userSelectedImage) }
+
+    val computerSelectedImage = remember { mutableStateOf(viewModel.computerSelectedImage) }
+    val result = remember {
+        mutableStateOf(String())
+    }
+    val wins by viewModel.wins.observeAsState()
+    val draws by viewModel.draws.observeAsState()
+    val losses by viewModel.losses.observeAsState()
+
+
+
     Column(
         modifier = Modifier
             .padding(16.dp)
@@ -49,59 +73,195 @@ fun PlayScreen(modifier : Modifier, navController : NavController) {
         )
         Text(
             text = stringResource(R.string.statistics),
-            style = MaterialTheme.typography.overline,
+            style = MaterialTheme.typography.h6,
             fontWeight = FontWeight(500),
             modifier = Modifier.padding(top = 16.dp, bottom = 16.dp)
         )
         Text(
-            text = "WIN: 1 DRAW: 1 LOSE: 1"
+            text = "WIN: $wins DRAW: $draws LOSE: $losses"
         )
-        Text(
-            text = "You win, change later",
-            style = MaterialTheme.typography.h4,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(top = 16.dp)
-        )
-            Row(
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.Bottom,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 150.dp, bottom = 48.dp),
+        Column(modifier = Modifier.height(60.dp)) {
+            if (!result.value.isNullOrEmpty()) {
+                Text(
+                    text = viewModel.checkWinner(
+                        userSelectedImage.value,
+                        computerSelectedImage.value
+                    ),
+                    style = MaterialTheme.typography.h4,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .height(40.dp)
+                )
+            }
+        }
 
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.Bottom,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp, bottom = 16.dp)
+                .height(155.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                computerSelectedImage.value?.let {
+                    Image(
+                        painter = painterResource(id = it),
+                        contentDescription = "Computer random image",
+                        modifier = Modifier
+                            .size(130.dp)
+                            .padding(top = 16.dp)
+                    )
+
+                }
+                Text(text = "Computer", style = MaterialTheme.typography.h6)
+            }
+
+            Text(
+                text = "V.S.",
+                style = MaterialTheme.typography.h4,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold
             )
-            {
-              Text(text = "Computer")
-                
-              Text(
-                  text = "V.S.",
-                  style = MaterialTheme.typography.h4,
-                  fontStyle = FontStyle.Italic,
-                  fontWeight = FontWeight.Bold
-                  )
-                
-              Text(text = "You")
-            }
-        
-            Row (
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.Bottom
-            ){
-                Card {
-                    Image(painter = painterResource(R.drawable.rock), contentDescription = "Rock")
-                }
-                Card {
-                    Image(painter = painterResource(id = R.drawable.paper), contentDescription = "Paper")
-                }
-                Card {
-                    Image(painter = painterResource(id = R.drawable.scissors), contentDescription = "Scissors",
-                        modifier = Modifier.clickable { navController.navigate(GameScreens.HistoryScreen.route) })
-                }
-            }
 
 
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                userSelectedImage.value?.let {
+                    Image(
+                        painter = painterResource(id = it),
+                        contentDescription = "Selected Image",
+                        modifier = Modifier
+                            .size(130.dp)
+                            .padding(top = 16.dp)
+
+                    )
+                }
+                Text(text = "You", style = MaterialTheme.typography.h6)
+            }
+        }
+
+
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 44.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Card {
+                Image(
+                    painter = painterResource(id = R.drawable.rock),
+                    contentDescription = "Rock",
+                    modifier = Modifier
+                        .rotate(-90F)
+                        .clickable {
+                            userSelectedImage.value = R.drawable.rock
+                            computerSelectedImage.value = viewModel.getRandomImage()
+                            viewModel.getDateAndTime()
+
+                            result.value = viewModel.checkWinner(
+                                userSelectedImage.value,
+                                computerSelectedImage.value
+                            )
+                            val newGame = verifyInputAndCorrect(viewModel.getDateAndTime(),
+                                computerSelectedImage.value!!, userSelectedImage.value!!
+                            )
+                            // TODO: insert the new game into the database!
+                            viewModel.insertGame(newGame)
+                        }
+                )
+            }
+            Card {
+                Image(
+                    painter = painterResource(id = R.drawable.paper),
+                    contentDescription = "Paper",
+                    modifier = Modifier
+                        .rotate(-90F)
+                        .clickable {
+                            userSelectedImage.value = R.drawable.paper
+                            computerSelectedImage.value = viewModel.getRandomImage()
+                            viewModel.getDateAndTime()
+                            result.value = viewModel.checkWinner(
+                                userSelectedImage.value,
+                                computerSelectedImage.value
+                            )
+                            val newGame = verifyInputAndCorrect(viewModel.getDateAndTime(),
+                                computerSelectedImage.value!!, userSelectedImage.value!!
+                            )
+                            // TODO: insert the new game into the database!
+                            viewModel.insertGame(newGame)
+                        }
+                )
+            }
+            Card {
+                Image(painter = painterResource(id = R.drawable.scissors),
+                    contentDescription = "Scissors",
+                    modifier = Modifier
+                        .rotate(-90F)
+                        .clickable {
+                            userSelectedImage.value = R.drawable.scissors
+                            computerSelectedImage.value = viewModel.getRandomImage()
+                            viewModel.getDateAndTime()
+                            result.value = viewModel.checkWinner(
+                                userSelectedImage.value,
+                                computerSelectedImage.value
+                            )
+                            val newGame = verifyInputAndCorrect(viewModel.getDateAndTime(),
+                                computerSelectedImage.value!!, userSelectedImage.value!!
+                            )
+                                // TODO: insert the new game into the database!
+                                viewModel.insertGame(newGame)
+                        }
+
+                )
+            }
+        }
+    }
+}
+fun verifyInputAndCorrect(
+    gameDate: Date,
+    computerChoice: Int,
+    userChoice: Int
+): Game {
+    val result: String
+
+    if (userChoice == computerChoice) {
+        result = "Draw"
+    } else if (
+        (userChoice == R.drawable.rock && computerChoice == R.drawable.scissors) ||
+        (userChoice == R.drawable.paper && computerChoice == R.drawable.rock) ||
+        (userChoice == R.drawable.scissors && computerChoice == R.drawable.paper)
+    ) {
+        result = "You win!"
+    } else {
+        result = "Computer wins!"
     }
 
+    // Create a Game object with the calculated result, game date, and choices
+    return Game(result, gameDate, computerChoice, userChoice)
+}
 
+
+
+//gameTitle = title
+//gamePlatform = platform
+//resultingGame = Game(gameTitle, gamePlatform, gameReleaseDate)
+//return resultingGame
+@Preview
+@Composable
+fun PreviewPlayScreen() {
+    PlayScreen(
+        modifier = Modifier,
+        navController = rememberNavController(),
+        viewModel = GameViewModel(Application())
+    )
 }
